418567c2ad750215ef4940fe6ba1b8f25873b728
Sujeet011 <sujeet7992370626@gmail.com>
2024-06-02 17:34:51 +0530

    Evaluation

---
+++ server.js
@@ -0,0 +1,17 @@
+// server.js
+import jsonServer from 'json-server';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+server.listen(5000, () => {
+  console.log('JSON Server is running on port 5000');
+});
